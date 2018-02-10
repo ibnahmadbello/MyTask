cdd32ec7bd8c0d597a70b501cc8039ebffde91c8
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-10 22:20:32 +0100

    Override onCreateOptionsMenu

--- app/src/main/java/com/regent/tech/mytask/AddNewTaskActivity.java
+++ app/src/main/java/com/regent/tech/mytask/AddNewTaskActivity.java
@@ -2,6 +2,8 @@ package com.regent.tech.mytask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AddNewTaskActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class AddNewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_add_task, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
