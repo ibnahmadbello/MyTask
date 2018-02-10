97feb513f0a5776803b6889177e821b795b91f49
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-10 21:58:36 +0100

    New Activity to add new task

---
+++ app/src/main/java/com/regent/tech/mytask/AddNewTaskActivity.java
@@ -0,0 +1,13 @@
+package com.regent.tech.mytask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_task);
+    }
+}
