ed72e58683d9204426005f9d35ff2857896f87b7
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-11 11:00:58 +0100

    Declared empty constructor, and override two method: onCreate and onUpgrade

--- app/src/main/java/com/regent/tech/mytask/Database/DatabaseSQLiteOpenHelper.java
+++ app/src/main/java/com/regent/tech/mytask/Database/DatabaseSQLiteOpenHelper.java
@@ -1,8 +1,30 @@
 package com.regent.tech.mytask.Database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by root on 2/11/18.
  */
 
-public class DatabaseSQLiteOpenHelper {
+public class DatabaseSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "task.db";
+    private static final int VERSION_NUMBER = 1;
+
+    //Constructor
+    public DatabaseSQLiteOpenHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION_NUMBER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
