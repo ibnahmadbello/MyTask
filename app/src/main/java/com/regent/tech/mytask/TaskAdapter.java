3adb79191b44e4f7d34db5db5fa7d414c781841e
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-11 12:33:04 +0100

    TaskAdapter.java

--- app/src/main/java/com/regent/tech/mytask/TaskAdapter.java
+++ app/src/main/java/com/regent/tech/mytask/TaskAdapter.java
@@ -1,8 +1,60 @@
 package com.regent.tech.mytask;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.regent.tech.mytask.model.Task;
+
+import java.util.List;
+
 /**
  * Created by root on 2/11/18.
  */
 
-public class TaskAdapter {
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<Task> taskList;
+
+    public TaskAdapter(Context context, List<Task> tasks){
+        this.mContext = context;
+        this.taskList = tasks;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView taskName;
+        TextView taskDate;
+        TextView taskState;
+
+        public ViewHolder(View view){
+            super(view);
+
+            taskName = (TextView) view.findViewById(R.id.task);
+            taskDate = (TextView) view.findViewById(R.id.date);
+            taskState = (TextView) view.findViewById(R.id.state_of_task);
+        }
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.each_row_task, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        //TODO: Need to bind the various views
+    }
+
+    @Override
+    public int getItemCount(){
+        //TODO: Need to return the size of the task
+    }
+
 }
---
+++ app/src/main/java/com/regent/tech/mytask/model/Task.java
@@ -0,0 +1,8 @@
+package com.regent.tech.mytask.model;
+
+/**
+ * Created by root on 2/11/18.
+ */
+
+public class Task {
+}
