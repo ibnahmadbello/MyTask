c3ecb47b05f6b6ff2052a0ea97bc026f9478f725
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-12 22:32:28 +0100

    New file: TaskDataSource.java (Wrapper class)

---
+++ app/src/main/java/com/regent/tech/mytask/Database/TaskDataSource.java
@@ -0,0 +1,11 @@
+package com.regent.tech.mytask.Database;
+
+/**
+ * Created by root on 2/12/18.
+ */
+
+public class TaskDataSource {
+
+    private static final String TAG =TaskDataSource.class.getSimpleName();
+
+}
