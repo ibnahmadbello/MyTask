6c235e69c1b04332d941a56cbb9d651aca16c645
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-13 20:21:34 +0100

    New File: TaskDataProvider.java (To insert dummy data)

---
+++ app/src/main/java/com/regent/tech/mytask/Database/TaskDataProvider.java
@@ -0,0 +1,19 @@
+package com.regent.tech.mytask.Database;
+
+import com.regent.tech.mytask.model.Task;
+
+import java.util.List;
+
+/**
+ * Created by root on 2/13/18.
+ */
+
+public class TaskDataProvider {
+
+    public static List<Task> taskList;
+
+    static {
+        taskList
+    }
+
+}
