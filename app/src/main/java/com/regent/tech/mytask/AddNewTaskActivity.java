624f5432ce82c6d2896b7132387e745bd9e1b842
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-14 16:50:07 +0100

    Casting the various view in the AddNewTaskActivity

--- app/src/main/java/com/regent/tech/mytask/AddNewTaskActivity.java
+++ app/src/main/java/com/regent/tech/mytask/AddNewTaskActivity.java
@@ -4,15 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddNewTaskActivity extends AppCompatActivity {
 
     private static final String TAG = AddNewTaskActivity.class.getSimpleName();
 
+    private EditText mTitleofTask;
+    private EditText mDetailofTask;
+    private DatePicker mDeadlineofTask;
+    private Spinner mStateofTask;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_task);
+
+        mTitleofTask = (EditText) findViewById(R.id.write_your_title_here);
+        mDetailofTask = (EditText) findViewById(R.id.type_your_details_here);
+        mDeadlineofTask = (DatePicker) findViewById(R.id.date_of_your_task);
+        mStateofTask = (Spinner) findViewById(R.id.spinner_yes_or_no);
+
+
+
     }
 
     @Override
