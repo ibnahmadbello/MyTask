bbaed1f7cfa690e867daa7c43bc5b642487d40d1
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-18 15:17:56 +0100

    implemented displayTask

--- app/src/main/java/com/regent/tech/mytask/MainActivity.java
+++ app/src/main/java/com/regent/tech/mytask/MainActivity.java
@@ -1,12 +1,18 @@
 package com.regent.tech.mytask;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.regent.tech.mytask.Database.DatabaseSQLiteOpenHelper;
+import com.regent.tech.mytask.Database.TaskContract;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +45,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayTask(){
+        //Create a database helper
+        DatabaseSQLiteOpenHelper helper = new DatabaseSQLiteOpenHelper(this);
 
+        //Get the database in read mode
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                TaskContract.TaskEntry._ID,
+                TaskContract.TaskEntry.COLUMN_TASK_NAME,
+                TaskContract.TaskEntry.COLUMN_TASK_DETAILS,
+                TaskContract.TaskEntry.COLUMN_TASK_DATE,
+                TaskContract.TaskEntry.COLUMN_TASK_STATUS };
+
+        // Perform a query on the pets table
+        Cursor cursor = db.query(
+                TaskContract.TaskEntry.TABLE_NAME, // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,                  // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
+
+        TextView displayTask = (TextView) findViewById(R.id.task_view);
+
+        try {
+            displayTask.setText("You have " + cursor.getCount() + "number of task undone.\n\n\n");
+
+            displayTask.append(TaskContract.TaskEntry._ID + "\t" +
+                    TaskContract.TaskEntry.COLUMN_TASK_NAME + "\t" +
+                    TaskContract.TaskEntry.COLUMN_TASK_DETAILS + "\t" +
+                    TaskContract.TaskEntry.COLUMN_TASK_DATE + "\t" +
+                    TaskContract.TaskEntry.COLUMN_TASK_STATUS + "\n\n");
+        }
     }
 
 }
